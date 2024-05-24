664d12909b099782205cd5cd739f10a423e3bc5c
Starl1t <93038650+Starl1t@users.noreply.github.com>
2024-05-24 22:40:02 +0300

    Update main1.cpp

--- Practice/Task 2/main1.cpp
+++ Practice/Task 2/main1.cpp
@@ -1,35 +1,35 @@
-//#include <iostream>
-//#include <thread>
-//#include <mutex>
-//
-//int global_variable = 0; // Глобальная переменная
-//
-//void increment_global(int id)
-//{
-//   for (int i = 0; i < 1000000; i++)
-//   {
-//      global_variable++;
-//   }
-//   std::cout << "Thread " << id << " finished work." << std::endl;
-//}
-//
-//int main() 
-//{
-//   std::thread threads[4]; // Массив из 4 потоков
-//
-//   // Создание 4 потоков и запуск функции increment_global в каждом
-//   for (int i = 0; i < 4; i++)
-//   {
-//      threads[i] = std::thread(increment_global, i);
-//   }
-//
-//   // Ожидание завершения всех потоков
-//   for (int i = 0; i < 4; i++)
-//   {
-//      threads[i].join();
-//   }
-//
-//   std::cout << "Global var: " << global_variable << std::endl;
-//
-//   return 0;
-//}
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+int global_variable = 0; // ГѓГ«Г®ГЎГ Г«ГјГ­Г Гї ГЇГҐГ°ГҐГ¬ГҐГ­Г­Г Гї
+
+void increment_global(int id)
+{
+   for (int i = 0; i < 1000000; i++)
+   {
+      global_variable++;
+   }
+   std::cout << "Thread " << id << " finished work." << std::endl;
+}
+
+int main() 
+{
+   std::thread threads[4]; // ГЊГ Г±Г±ГЁГў ГЁГ§ 4 ГЇГ®ГІГ®ГЄГ®Гў
+
+   // Г‘Г®Г§Г¤Г Г­ГЁГҐ 4 ГЇГ®ГІГ®ГЄГ®Гў ГЁ Г§Г ГЇГіГ±ГЄ ГґГіГ­ГЄГ¶ГЁГЁ increment_global Гў ГЄГ Г¦Г¤Г®Г¬
+   for (int i = 0; i < 4; i++)
+   {
+      threads[i] = std::thread(increment_global, i);
+   }
+
+   // ГЋГ¦ГЁГ¤Г Г­ГЁГҐ Г§Г ГўГҐГ°ГёГҐГ­ГЁГї ГўГ±ГҐГµ ГЇГ®ГІГ®ГЄГ®Гў
+   for (int i = 0; i < 4; i++)
+   {
+      threads[i].join();
+   }
+
+   std::cout << "Global var: " << global_variable << std::endl;
+
+   return 0;
+}
